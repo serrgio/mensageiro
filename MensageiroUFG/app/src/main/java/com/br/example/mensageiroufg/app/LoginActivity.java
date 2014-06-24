5a6c44188900852bb66e4e91ba6d3d5ccebfd0e5
José Sérgio de Souza <serrgio@gmail.com>
2014-06-24 20:58:20 -0300

    MensageiroUFG

---
+++ MensageiroUFG/app/src/main/java/com/br/example/mensageiroufg/app/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.br.example.mensageiroufg.app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void entrar(View view) {
+        Intent intent = new Intent(this, lstMensagensActivity.class);
+        startActivity(intent);
+        EditText login = (EditText) findViewById(R.id.editText);
+        EditText senha = (EditText) findViewById(R.id.editText2);
+        String message = login.getText().toString();
+        String message2 = senha.getText().toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
